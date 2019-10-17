dca9086a7c4751f08031f0d857967630ed857ff3
Chumachenko Roman <rom4ik730@gmail.com>
2019-10-17 11:47:05 +0300

    Implemented Glide image loading. Updated ArticleActivity layout.

--- app/src/main/java/com/legion1900/simplenews/views/activities/ArticleActivity.java
+++ app/src/main/java/com/legion1900/simplenews/views/activities/ArticleActivity.java
@@ -3,14 +3,16 @@ package com.legion1900.simplenews.views.activities;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 import com.legion1900.simplenews.R;
 import com.legion1900.simplenews.networking.data.Article;
 import com.squareup.picasso.Picasso;
@@ -21,7 +23,11 @@ public class ArticleActivity extends AppCompatActivity {
 
     public static final String EXTRA_ARTICLE = "com.legion1900.simplenews.ArticleActivity";
 
-    private ImageView image;
+    private LinearLayout blockPicasso;
+    private LinearLayout blockGlide;
+
+    private ImageView imagePicasso;
+    private ImageView imageGlide;
     private TextView title;
     private TextView source;
     private TextView description;
@@ -40,7 +46,10 @@ public class ArticleActivity extends AppCompatActivity {
 
     private void initViews() {
         Resources r = getResources();
-        image = findViewById(R.id.image);
+        blockPicasso = findViewById(R.id.block_picasso);
+        blockGlide = findViewById(R.id.block_glide);
+        imagePicasso = findViewById(R.id.image_picasso);
+        imageGlide = findViewById(R.id.image_glide);
         title = findViewById(R.id.tv_title);
         source = findViewById(R.id.tv_source_name);
         description = findViewById(R.id.tv_description);
@@ -49,12 +58,20 @@ public class ArticleActivity extends AppCompatActivity {
         Article article = intent.getParcelableExtra(EXTRA_ARTICLE);
         if (article != null) {
             String imgUrl = article.getUrlToImage();
-            if (imgUrl == null)
-                image.setVisibility(View.GONE);
-            else picasso.load(imgUrl)
-                    .placeholder(R.drawable.ic_image_24dp)
-                    .error(R.drawable.ic_broken_image_24dp)
-                    .into(image);
+            if (imgUrl == null) {
+                blockPicasso.setVisibility(View.GONE);
+                blockGlide.setVisibility(View.GONE);
+            }
+            else {
+                picasso.load(imgUrl)
+                        .placeholder(R.drawable.ic_image_24dp)
+                        .error(R.drawable.ic_broken_image_24dp)
+                        .into(imagePicasso);
+                Glide.with(this).load(imgUrl)
+                        .placeholder(R.drawable.ic_image_24dp)
+                        .error(R.drawable.ic_broken_image_24dp)
+                        .into(imageGlide);
+            }
             String titleText = buildFieldValue(
                     r.getString(R.string.title_default),
                     article.getTitle()
